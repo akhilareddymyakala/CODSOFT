4d0ffdb36eff733e72253f0cbbadf90d085e9867
akhilareddymyakala <akhilareddymyakala@gmail.com>
2026-05-20 11:27:37 +0530

    Add files via upload

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -4,22 +4,42 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int subjects = 5;
+
+        System.out.print("Enter Student Name: ");
+        String name=sc.nextLine();
+
+        System.out.print("Enter Number of Subjects: ");
+        int subjects = sc.nextInt();
+        sc.nextLine();
+        
         int total = 0;
 
         for (int i = 1; i <= subjects; i++) {
-            System.out.print("Enter marks " + i + ": ");
-            total += sc.nextInt();
+            System.out.print("Enter Subject Name: ");
+            String subjectName = sc.nextLine();
+
+            System.out.print("Enter marks in " + subjectName + ": ");
+            int marks = sc.nextInt();
+
+            sc.nextLine();
+            
+            total =total+marks;
         }
 
         double avg = total / (double) subjects;
 
-        System.out.println("Total: " + total);
+        System.out.println("\nStudent Name: " +name );
+        System.out.println("Total Marks: " + total);
         System.out.println("Average: " + avg);
 
-        if (avg >= 90) System.out.println("Grade A");
-        else if (avg >= 75) System.out.println("Grade B");
-        else if (avg >= 50) System.out.println("Grade C");
-        else System.out.println("Fail");
+        if (avg >= 90){
+            System.out.println("Grade A");
+        }else if (avg >= 75){
+             System.out.println("Grade B");
+        }else if (avg >= 50) {
+            System.out.println("Grade C");
+         } else {
+            System.out.println("Fail");
+         }
     }
 }
--- NumberGame.java
+++ NumberGame.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class NumberGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
+        Random random = new Random();
 
-        int number = rand.nextInt(100) + 1;
+        int number = random.nextInt(100) + 1;
         int guess = 0;
+        
 
         while (guess != number) {
-            System.out.print("Enter guess: ");
+            System.out.print("Enter your guessing number: ");
             guess = sc.nextInt();
 
-            if (guess < number)
+            if (guess < number){
                 System.out.println("Too low");
-            else if (guess > number)
+            }else if (guess > number){
                 System.out.println("Too high");
-            else
-                System.out.println("Correct!");
+            }else{
+                System.out.println(" your guess is Correct!");
+            }
         }
     }
 }
